a386760aa6e0e66e9a930118f6e7c5dc74c3147c
遠藤慎 <end3mame@gmail.com>
2023-05-20 11:47:18 +0900

    model

---
+++ unit_4/lesson_18/recipe_app/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose"),
+  { Schema } = mongoose;
+
+// ユーザのスキーマを作る
+const userSchema = new Schema(
+  {
+    name: {
+      first: {
+        type: String,
+        trim: true,
+      },
+      last: {
+        type: String,
+        trim: true,
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    zipCode: {
+      type: Number,
+      min: [10000, "Zip code too short"],
+      max: 99999,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    courses: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    subscribedAccouont: {
+      type: Schema.Types.ObjectId,
+      ref: "Subscriber",
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+// ユーザーのフルネームを取得する仮想属性の追加
+userSchema.virtual("fullname").get(function () {
+  return `${this.name.first}${this.name.last}`;
+});
+
+module.exports = mongoose.model("User", userSchema);
